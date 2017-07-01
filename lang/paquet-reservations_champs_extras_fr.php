8276b759811bbbab54861f08a01e6e4ab4fc89a0
Rainer <rainer@websimple.be>
2017-07-01 13:07:13 +0200

    Import initial

---
+++ lang/paquet-reservations_champs_extras_fr.php
@@ -0,0 +1,18 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// R
+	'reservations_champs_extras_description' => 'Les champs extras auteur et reservation sont déjà nativement intégré dans le plugin Réservation Evénements.
+
+Ce plugin ajoute la possibilité d\'adapter leur comportement par rapport à leur définition initiale, définis via le plugin Champs extras. Permettant ainsi de:
+- Avoir un comportement différent pour les champs extras auteurs pour le formulaire reservation par rapport aux autres formulaires auteurs natives.
+- Adapter spécifiquement les champs extras définis pour chaque formulaire reservation',
+	'reservations_champs_extras_nom' => 'Réservations Champs Extras',
+	'reservations_champs_extras_slogan' => 'Permet d\'adapter le comportement des champs extras auteur et reservation',
+);
---
+++ reservations_champs_extras_pipelines.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Utilisations de pipelines par Réservations Champs Extras
+ *
+ * @plugin     Réservations Champs Extras
+ * @copyright  2017
+ * @author     Rainer
+ * @licence    GNU/GPL
+ * @package    SPIP\Reservations_champs_extras\Pipelines
+ */
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+
+/*
+ * Un fichier de pipelines permet de regrouper
+ * les fonctions de branchement de votre plugin
+ * sur des pipelines existants.
+ */
