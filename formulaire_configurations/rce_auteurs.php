f08ac6c9edb173658f7fb10cc350d78d897c6145
abelass <rainer@websimple.be>
2017-07-04 16:20:36 +0200

    faudrait indiquer le traitement spécifique et se baser sur saisies

--- formulaire_configurations/rce_auteurs.php
+++ formulaire_configurations/rce_auteurs.php
@@ -11,6 +11,12 @@
  */
 
 function formulaire_configurations_rce_auteurs_dist($valeurs) {
+	include_spip('cextras_pipelines');
+	$champs_extras = champs_extras_objet(table_objet_sql('auteur'));
+
+	include_spip('inc/iextras');
+	$saisies = iextras_champs_extras_definis( table_objet_sql('auteur') );
+
 	return array (
 		'nom' => _T('reservations_champs_extras:nom_formulaire_configuration_champs_extras_auteurs'),
 		'saisies' => array (
@@ -22,7 +28,9 @@ function formulaire_configurations_rce_auteurs_dist($valeurs) {
 					'obligatoire' => 'oui'
 				),
 			),
+			$saisies,
 		),
+		'champs_extras' => $saisies,
 	);
 
 }
