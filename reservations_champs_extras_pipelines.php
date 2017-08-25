c89c1fb19f799d01b178e7f7135213d0847a19a1
abelass <rainer@websimple.be>
2017-08-25 16:29:16 +0200

    intervenir sur le squeletter generer_saisies
    pour controler l'affichage dans le formulaire d'edition, début des
    travaux

--- reservations_champs_extras_pipelines.php
+++ reservations_champs_extras_pipelines.php
@@ -12,3 +12,23 @@
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
+
+function reservations_champs_extras_recuperer_fond($flux) {
+	$fond = $flux['args']['fond'];
+	$contexte = $flux['data']['contexte'];
+	//print $fond;
+	if ($fond == 'inclure/generer_saisies' &&
+			isset($contexte['_action']) &&
+			isset($contexte['_action'][0]) &&
+			$contexte['_action'][0] == 'editer_reservation') {
+				print '<pre>';
+				//print_r($contexte);
+				print '</pre>';
+				$champs_extras = $contexte['saisies'];
+
+		//$contexte['saisies'] = rce_configuration_charger($champs_extras, $configuration, $objet);
+
+	}
+
+	return $flux;
+}
