1b9c7ed76923e3e47ddb54dd017bf5f73aeacc14
abelass <rainer@websimple.be>
2017-08-29 11:44:59 +0200

    optimiser le code

---
+++ formulaire_configurations/inclure/affichage_rce_fonctions.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Fonctions pour affichage_rce.html
+ *
+ * @plugin     Réservations Champs Extras
+ * @copyright  2017
+ * @author     Rainer Müller
+ * @licence    GNU/GPL
+ * @package    SPIP\Reservations_champs_extras\Formulaire configurations
+ */
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+include_spip('inc/reservations_formulaires');
+include_spip('inc/cextras');
+
+/**
+ * Attribue le label à un champ
+ *
+ * @param  string $objet objet spip.
+ *
+ * @return array       Données du pipeline
+ */
+function rce_noms_champs($objet) {
+
+	$saisies = cextras_obtenir_saisies_champs_extras($objet);
+
+	$champs = array();
+	foreach ($saisies AS $champ => $donnees) {
+		$champs[$champ] = $donnees['options']['label'];
+	}
+
+	return $champs;
+}
